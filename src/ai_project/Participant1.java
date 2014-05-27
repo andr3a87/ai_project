fb4f813ffb660a7f910102f8fe67ddfb6767c470
ANDREA LATELLA <andrea87@andrea.latella87@msn.com>
2014-05-27 15:46:41 +0200

    Creato Participant1

---
+++ src/ai_project/Participant1.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ai_project;
+
+import jade.core.AID;
+import jade.core.Agent;
+import jade.core.behaviours.Behaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAException;
+import jade.lang.acl.ACLMessage;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Andrea
+ */
+public class Participant1 extends Agent {
+    protected void setup() {
+        DFAgentDescription dfd =new DFAgentDescription();
+        dfd.setName(getAID());
+        ServiceDescription sd = new ServiceDescription();
+        sd.setType("type1");
+        sd.setName("JADE-Type1");
+        dfd.addServices(sd);
+        try{
+            DFService.register(this, dfd);
+        }catch(FIPAException fe) {
+            fe.printStackTrace();
+        }
+        addBehaviour(new Behaviour() {
+            private int step = 0;
+
+            @Override
+            public void action() {
+                switch(step) {
+                  case 0:
+                  break;
+                  case 1:
+                  break;
+                  case 2:
+                  break;
+                }
+            }
+
+              @Override
+              public boolean done() {
+                  throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+              }
+        });
+    }
+}
